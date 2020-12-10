a4ffe82082d6af9a77de6c70e596bb2c2925edd2
lolotetsoulo <logan0408199@gmail.com>
2020-12-10 17:31:30 +0100

    historique transaction db

--- MyMoneyManager/app/src/main/java/com/helha/mymoneymanager/fragment/HistoriqueFragment.java
+++ MyMoneyManager/app/src/main/java/com/helha/mymoneymanager/fragment/HistoriqueFragment.java
@@ -1,8 +1,11 @@
 package com.helha.mymoneymanager.fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +19,7 @@ import java.util.List;
 
 import model.transaction.TransactionHistory;
 import model.transaction.TransactionHistoryAdapter;
+import repository.TransactionRepository;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -38,7 +42,8 @@ public class HistoriqueFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =inflater.inflate(R.layout.fragment_historique, container, false);
-
+        final String userToken = getTokenShared();
+        TransactionRepository transactionRepository = new TransactionRepository();
         ListView lvTransaction = view.findViewById(R.id.lv_historique);
 
         //Valeur à modifié avec les valeurs en db
@@ -47,6 +52,14 @@ public class HistoriqueFragment extends Fragment {
        // transactions.add(new TransactionHistory("9\n sep","transfert test","moi","lui", (float) 10.0));
         //transactions.add(new TransactionHistory("10\n sep","transfert test2","moi","lui",(float) 10.0));
         //*********** END ***************
+
+        transactionRepository.query(userToken).observe(this.getViewLifecycleOwner(), new Observer<List<TransactionHistory>>() {
+            @Override
+            public void onChanged(List<TransactionHistory> transactionHistories) {
+                transactions.addAll(transactionHistories);
+            }
+        });
+
         TransactionHistoryAdapter transactionHistoryAdapter = new TransactionHistoryAdapter(
                 getContext(),
                 R.id.lv_historique,
@@ -55,5 +68,13 @@ public class HistoriqueFragment extends Fragment {
 
         lvTransaction.setAdapter(transactionHistoryAdapter);
         return view;
+    }
+
+    public String getTokenShared()
+    {
+        //Reception du SHARED PREFERENCE disponible et recopie du userToken dans le fragment.
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("USERTOKENSHARED", Context.MODE_PRIVATE);
+
+        return preferences.getString("TOKEN", "No Token");
     }
 }
